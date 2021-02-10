2b225b444058207f5bcc7d600a956c5572720bc7
Ubuntu <ubuntu@ip-172-31-2-238.eu-west-1.compute.internal>
2021-02-10 12:13:00 +0000

    cluster

--- instalaciones/clusterKC_sec/crearCertificados.sh
+++ instalaciones/clusterKC_sec/crearCertificados.sh
@@ -3,3 +3,4 @@ mkdir -p /home/ubuntu/environment/data/certificados
 chmod 777 -R /home/ubuntu/environment/data/certificados
 docker-compose run --rm crear_certificados
 unzip /home/ubuntu/environment/data/certificados/certificados.zip -d /home/ubuntu/environment/data/certificados/
+chmod 777 -R /home/ubuntu/environment/data/certificados
